fd4321725e82f08aefae2953beb8a3e1aec82cf1
Johan Bregeon <johan.bregeon@gmail.com>
2018-04-18 12:42:47 +0200

    use get_data

--- ctapipe/atmosphere/plot_radiosonde.py
+++ ctapipe/atmosphere/plot_radiosonde.py
@@ -6,18 +6,18 @@ plot radiosonde data
 @author: Johan Bregeon
 """
 
-# @TODO 
+# @TODO
 # * plot all samples for P and T on one graph
 # * plot Dewpoint, Wind Dir and Wind Speed
 
-RADIOSONDE_FILEPATH='../ctapipe-extra/ctapipe_resources/radiosonde_68110_2009_2014.fsl'
-
+from ctapipe.utils import get_dataset
 from matplotlib import pyplot as plt
 
 from ctapipe.atmosphere.radiosonde_reader import pRadioSondeReader
 
+# Read input data
 reader=pRadioSondeReader()
-reader.readFile(RADIOSONDE_FILEPATH)
+reader.readFile(get_dataset('radiosonde_68110_2009_2014.fsl'))
 prof=reader.getSounding('2009-08-23')
 prof.dump()
 
@@ -28,7 +28,7 @@ pressure, = ax.plot(prof.Pressure, prof.Height, color='blue')
 temp,     = ax.plot(prof.Temperature, prof.Height, color='green')
 legend_handles = [pressure, temp]
 legend_labels = ['Pressure', 'Temperature']
-  
+
 
 plt.title('Atmospheric (P, T) Profiles')
 ax.set_xlabel("Pressure, Temperature")
